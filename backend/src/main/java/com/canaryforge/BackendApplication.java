29eeb79e7f3f63a4674302160106c3a85eaf9535
Odd1ty <mariofernandezsantos@gmail.com>
2025-08-16 21:39:17 +0200

    refactor(app-i1-url-cebo): move BackendApplication to root package for proper component scan

--- backend/src/main/java/com/canaryforge/BackendApplication.java
+++ backend/src/main/java/com/canaryforge/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.canaryforge.backend;
+package com.canaryforge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
